064ae0a21756e04c75c4989988d5bafe475c746a
Gianpaul Rachiele <gianpaul817@gmail.com>
2018-01-12 15:41:21 -0500

    added email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :email, :first_name, :last_name, :password, presence: true
-  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message:"That's not a valid email."}
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: "That's not a valid email."}
   has_secure_password
   has_many :posts
 

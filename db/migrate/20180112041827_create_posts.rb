69d29dc1fa5a1c1055039ae25117bbe6294b35c3
Gianpaul Rachiele <gianpaul817@gmail.com>
2018-01-12 12:57:21 -0500

    added location_id column to poststable

--- db/migrate/20180112041827_create_posts.rb
+++ db/migrate/20180112041827_create_posts.rb
@@ -3,6 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.1]
     create_table :posts do |t|
       t.integer :user_id
       t.integer :category_id
+      t.integer :location_id
       t.string :title
       t.text :description
       t.timestamps
